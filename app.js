0b21274fe3e4862b805e59590564871cc1dcd611
rakeshramesh98 <rakesh98raji@gmail.com>
2021-02-12 19:34:07 +0530

    value,unshift

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
-
-// const arrayFill = require('./arrayFill.js'); 
-const unshift = require('./unshift');
-const value = require('./value'); 
-
-
-// arrayFill()
-
-value();
-unshift();
-=======
 const arrayFill = require('./arrayFill.js'); 
 const concat = require('./concat.js');
 const copyWithin = require('./copyWithin.js');
 const entries = require('./entries.js');
 const every = require('./every.js');
 const Iterate = require('./Iterate.js'); 
+const unshift = require('./unshift');
+const value = require('./value'); 
 arrayFill()
 Iterate()
 concat()
 copyWithin()
 entries()
 every()
->>>>>>> 05aa3e8088941c902ba846f9bfbae7cd3d6f08f7
+value();
+unshift();
