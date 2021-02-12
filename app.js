cf0064b0f01ed63591eae3ca8387f4cf68d31edb
jeevan <jeevanchadarla1998@gmail.com>
2021-02-12 19:27:35 +0530

    value,unshift

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const arrayFill = require('./arrayFill.js'); 
-arrayFill()
+
+// const arrayFill = require('./arrayFill.js'); 
+const unshift = require('./unshift');
+const value = require('./value'); 
+
+
+// arrayFill()
+
+value();
+unshift();
