05aa3e8088941c902ba846f9bfbae7cd3d6f08f7
rakeshramesh98 <rakesh98raji@gmail.com>
2021-02-12 19:18:10 +0530

    fill, concat, copywithin, entries, every, iterate

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const arrayFill = require('./arrayFill.js'); 
+const concat = require('./concat.js');
+const copyWithin = require('./copyWithin.js');
+const entries = require('./entries.js');
+const every = require('./every.js');
+const Iterate = require('./Iterate.js'); 
 arrayFill()
+Iterate()
+concat()
+copyWithin()
+entries()
+every()
