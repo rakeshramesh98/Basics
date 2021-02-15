15e88dc0eac3909a57fe725c19bcc0556f0c87ac
rakeshramesh98 <rakesh98raji@gmail.com>
2021-02-15 13:58:50 +0530

    import changes

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-
-const arrayFill = require('./arrayFill.js'); 
 const unshift = require('./unshift');
 const value = require('./value'); 
-
 const arrayreverse = require("./arrayreverse");
 const arrayshift = require("./arrayshift");
 const arrayslice = require("./arrayslice");
@@ -11,18 +7,9 @@ const arraysome = require("./arraysome");
 const arraysort = require("./arraysort");
 const map = require("./map");
 const reduce = require("./reduce");
-
 const arraySplice = require("./array.splice");
 const arraytostring = require("./arraytostring");
-
-
-
-
-value();
-unshift();
-=======
 const arrayFill = require('./arrayFill.js'); 
->>>>>>> 554807c61b40c1823f148ff8737cab8cb5a95421
 const concat = require('./concat.js');
 const copyWithin = require('./copyWithin.js');
 const entries = require('./entries.js');
@@ -38,27 +25,7 @@ const includes = require('./includes.js');
 const indexOf = require('./indexOf.js');
 const isArray = require('./isArray.js');
 const Iterate = require('./Iterate.js'); 
-<<<<<<< HEAD
-arrayFill()
-Iterate()
-concat()
-copyWithin()
-entries()
-every()
-/////////////////////////////////////////////from 26 change code in respective files
-arraytostring();
-arraySplice();
-arraysort();
-arraysome();
-arrayslice();
-arrayshift();
-arrayreverse();
-reduce();
-map();
-=======
 const join = require('./join.js');
-const unshift = require('./unshift');
-const value = require('./value'); 
 arrayFill();
 Iterate();
 concat();
@@ -78,4 +45,22 @@ includes();
 indexOf();
 isArray();
 join();
->>>>>>> 554807c61b40c1823f148ff8737cab8cb5a95421
+arrayFill()
+Iterate()
+concat()
+copyWithin()
+entries()
+every()
+arraytostring();
+arraySplice();
+arraysort();
+arraysome();
+arrayslice();
+arrayshift();
+arrayreverse();
+reduce();
+map();
+value();
+unshift();
+
+
--- unshift.js
+++ unshift.js
@@ -8,23 +8,5 @@ module.exports = function unshift() {
     console.log('display the entire array');
     console.log(array1);
     // expected output: Array [4, 5, 1, 2, 3]
-<<<<<<< HEAD
-    
-
-    let arr = [4, 5, 6]
-    arr.unshift(1, 2, 3)
-    console.log(arr);
-// [1, 2, 3, 4, 5, 6]
-
-    arr = [4, 5, 6] // resetting the array
-
-    arr.unshift(1)
-    arr.unshift(2)
-    arr.unshift(3)
-
-console.log(arr)
-// [3, 2, 1, 4, 5, 6]
-=======
     console.log("----------------------------------------------------")
->>>>>>> 554807c61b40c1823f148ff8737cab8cb5a95421
 }
